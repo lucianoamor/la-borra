a4d739a773ff40380cbdab8205de39f2fda7cdb4
lucianoamor <lucianoamor@gmail.com>
2015-12-11 13:16:49 -0300

    tbl resultados: un decimal siempre (inc .0) y coma en vez de punto

--- resultados.php
+++ resultados.php
@@ -126,14 +126,14 @@ foreach ($avg as $k => $v) {
         <td class="text-center">
             <img src="'.$candidatos[$k]["imagen"].'" title="'.$candidatos[$k]["nombre"].'" alt="'.$candidatos[$k]["nombre"].'" class="img" style="border-color:#'.$candidatos[$k]["color"].';" data-content="<small>'.$candidatos[$k]["agrupacion"].'</small>" data-clase="c-'.$k.'" />
         </td>
-        <td class="text-center td-col">'.$v.'</td>
-        <td class="text-center td-col">'.$min[$k].'</td>
-        <td class="text-center td-col">'.$max[$k].'</td>
-        <td class="text-center td-col">'.$cv[$k].'</td>
+        <td class="text-center td-col">'.number_format($v, 1, ",", "").'</td>
+        <td class="text-center td-col">'.number_format($min[$k], 1, ",", "").'</td>
+        <td class="text-center td-col">'.number_format($max[$k], 1, ",", "").'</td>
+        <td class="text-center td-col">'.number_format($cv[$k], 1, ",", "").'</td>
         <td class="text-center td-col">'.$n1[$k].'</td>
         <td class="text-center td-col">'.$n[$k].'</td>
         <td class="text-center td-col"><button type="button" class="btn btn-default btn-xs btn-hide-circles" data-candidato="'.$k.'"><i class="icon-check"></i></button><br/><button type="button" class="btn btn-default btn-xs btn-hide-lines" data-candidato="'.$k.'"><i class="icon-check"></i></button></td>
-        <td class="resultado text-center td-col">'.$resultados[$k].'</td>
+        <td class="resultado text-center td-col">'.(is_numeric($resultados[$k]) ? number_format($resultados[$k], 1, ",", "") : $resultados[$k]).'</td>
     </tr>';
 }
 
